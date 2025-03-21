8e187ab5fc79fa04d4c42164b9700704606c7ac8
josueorbiware <jvital@orbiware.com>
2025-03-20 23:34:04 -0500

    first commit

---
+++ academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy Learning Odoo",
+
+    'summary': """
+        Academy Learning Odoo""",
+
+    'description': """
+        This is a course learning Odoo app by Josué.
+    """,
+
+    'author': "Josué Vital",
+    'website': "https://www.yourcompany.com",
+
+    'category': 'Custom Modules/Tech Training',
+    'version': '16.0.0.1',
+    'license': 'OPL-1',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+}
---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Motorcycle Registry",
+
+    'summary': """
+        Manage Registration of Motorcycles""",
+
+    'description': """
+        Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership
+        of each motorcycled of the brand.
+    """,
+
+    'author': "Josué Vital",
+    'website': "https://www.yourcompany.com",
+
+    'category': 'Kawiil/Custom Module',
+    'version': '16.0.0.1',
+    'license': 'OPL-1',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+}
